6621bf7356a265eeb8d20f09425369ea78205602
ibraheem ibraheem <iibraheem2001@gmail.com>
2021-09-28 17:45:27 -0700

    fixed bugs related to the app.js code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const productImageSectionTag = document.getElementById('all_prodcuts');
-const leftproductImageTag = document.getElementById('left_product_img');
-const rightproductImageTag = document.getElementById('right_prodcut_img');
+const productImageSectionTag = document.getElementById('all_products');
+const leftProductImageTag = document.getElementById('left_product_img');
+const rightProductImageTag = document.getElementById('right_product_img');
 
 
 
 let totalClicks = 0;
 
-let leftproductOnTePage = null;
+let leftproductOnThePage = null;
 let rightproductOnThePage = null;
 
 const ProductPicture = function (name, imageSrc) {
@@ -22,6 +22,8 @@ ProductPicture.allImages = [];
 
 
 const renderNewProducts = function (leftIndex, rightIndex){
+
+
   leftProductImageTag.src = ProductPicture.allImages[leftIndex].url;
   rightProductImageTag.src = ProductPicture.allImages[rightIndex].url;
 };
@@ -32,17 +34,15 @@ const pickNewProducts = function(){
   do {
     rightIndex = Math.floor(Math.random() * ProductPicture.allImages.length);
   } while (rightIndex === leftIndex);
-  console.log(ProductPicture.allImages[leftIndex].name, ProductPicture.allImages[rightIndex].name);
 
-  lefProductOnThePage = ProductPicture.allImages[leftIndex];
+  leftProductOnThePage = ProductPicture.allImages[leftIndex];
   rightProductOnThePage = ProductPicture.allImages[rightIndex];
 
   renderNewProducts(leftIndex, rightIndex);
 };
 
 const handleClickOnProduct = function(event){
-  console.log('im still alive');
-  if(totalClicks < 5){
+  if(totalClicks < 25){
 
     const thingWeClickedOn = event.target;
     const id = thingWeClickedOn.id;
@@ -58,15 +58,15 @@ const handleClickOnProduct = function(event){
 
       leftProductOnThePage.timesShown++;
       rightProductOnThePage.timesShown++;
-
+    console.log(leftProductOnThePage);
       pickNewProducts();
     }
-    console.log(event.target.id);
+
   }
   totalClicks++;
-  if(totalClicks === 5){
+  if(totalClicks === 25){
     productImageSectionTag.removeEventListener('click', handleClickOnProduct);
-    console.log('you picked 5 products, thanks!');
+    console.log('you picked 25 products, thanks!');
 
   }
 };
@@ -80,17 +80,17 @@ productImageSectionTag.addEventListener('click', handleClickOnProduct);
 
 
 new ProductPicture('Bubblegum', 'img/assets/bubblegum.jpg');
-new ProductPicture('Bubblegum', 'img/assets/banana.jpg');
-new ProductPicture('Bubblegum', 'img/assets/chair.jpg');
-new ProductPicture('Bubblegum', 'img/assets/sweep.jpg');
-new ProductPicture('Bubblegum', 'img/assets/water-can.jpg');
-new ProductPicture('Bubblegum', 'img/assets/wine-glass.jpg');
-new ProductPicture('Bubblegum', 'img/assets/dragon.jpg');
-new ProductPicture('Bubblegum', 'img/assets/shark.jpg');
-new ProductPicture('Bubblegum', 'img/assets/breakfast.jpg');
-new ProductPicture('Bubblegum', 'img/assets/scissors.jpg');
-new ProductPicture('Bubblegum', 'img/assets/unicorn.jpg');
-new ProductPicture('Bubblegum', 'img/assets/boots.jpg');
+new ProductPicture('Banana', 'img/assets/banana.jpg');
+new ProductPicture('Chair', 'img/assets/chair.jpg');
+new ProductPicture('Sweep', 'img/assets/sweep.png');
+new ProductPicture('Water-can', 'img/assets/water-can.jpg');
+new ProductPicture('Wine-glass', 'img/assets/wine-glass.jpg');
+new ProductPicture('Dragon', 'img/assets/dragon.jpg');
+new ProductPicture('Shark', 'img/assets/shark.jpg');
+new ProductPicture('Breakfast', 'img/assets/breakfast.jpg');
+new ProductPicture('Scissors', 'img/assets/scissors.jpg');
+new ProductPicture('Unicorn', 'img/assets/unicorn.jpg');
+new ProductPicture('Boots', 'img/assets/boots.jpg');
 
 
 
